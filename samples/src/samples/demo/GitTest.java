28787afd960eb93def4762e563c062344f50cf7e
firzanahuda <hudafirzana@gmail.com>
2022-03-11 12:20:27 +0800

    Initial commit

---
+++ samples/src/samples/demo/GitTest.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class GitTest {
+	
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
